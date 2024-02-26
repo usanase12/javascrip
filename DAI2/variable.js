9c122c3ede5fbc98fc489960792ed30d98c4f413
usanase12 <usanasesandra22@gmail.com>
2024-02-26 13:14:49 +0200

    variables and datatype in js

---
+++ DAI2/variable.js
@@ -0,0 +1,48 @@
+// going to focus on variables and data
+// 1. numeric variables
+// 2.string variables
+// 3.boolean variables
+// 5.array varriables
+const age= 24;//number type variables
+const name= "sandrine";
+console.log("My name is " + name + " and I'm " + age + "years old");
+console.log('My name is $(name) and am $(age) years old');
+//console.log(Array);
+// console.log(myFavFood);
+/*
+variables declarations and variable definition
+*/
+var x;//this is variable declaration
+var y = "shecancode";//declaration and definition
+
+//function
+function myLastName(){
+    let me = "sando";
+    console.log(me);
+}
+myLastName();
+// Difference between Primitive Data Types and Object Data Types:
+//primitive data types:
+//Eg: number, string, boolean, null, underfined
+
+//object Data Type:
+//Stored as a reference in the variable's memory location
+//eg: object, array, function
+
+//undefined
+let Str = "sando"
+console.log()
+
+//NULL
+console.log(typeof me);
+let ma = null;
+console.log(ma);
+
+//NaN(not a number)
+let str = "hello";
+let Age = 12;
+console.log(str % Age);
+
+
+
+
